4e360512e6bf98e29fccb581a76eef18c0cc3d09
Jesse-Tingle <jtingle0@gmail.com>
2019-10-27 06:05:18 -0400

    install CRA, set up basic boilerplate, and h1 tag in App.js

---
+++ superhero-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>All the information you could ever need on every Superhero and Villain!</h1>
+    </div>
+  );
+}
+
+export default App;
